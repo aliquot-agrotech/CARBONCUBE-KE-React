0d960e12f48605cd3d79c06650a94a5567e78b08
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-08-15 12:06:51 +0300

    Styles up the order modal

--- src/admin/OrdersManagement.js
+++ src/admin/OrdersManagement.js
@@ -133,7 +133,7 @@ const OrdersManagement = () => {
                                 <Card.Header className="text-center justify-content-center">
                                     Orders
                                 </Card.Header>
-                                    <Card.Body>
+                                    <Card.Body className="p-0">
                                     <Table hover className="orders-table text-center">
                                         <thead className="table-header">
                                             <tr>
@@ -228,42 +228,65 @@ const OrdersManagement = () => {
                                 </Modal.Header>
                                 <Modal.Body>
                                     {selectedOrder ? (
-                                        <div>
-                                            <div className="order-details mb-4 text-center">
-                                                <div className="order-detail-item">
-                                                    <strong>Order ID:</strong> {selectedOrder.id}
-                                                </div>
-                                                <div className="order-detail-item">
-                                                    <strong>Purchaser:</strong> {selectedOrder.purchaser?.fullname || 'Unknown'}
-                                                </div>
-                                                <div className="order-detail-item">
-                                                    <strong>Date Ordered:</strong> {selectedOrder.order_date || 'N/A'}
-                                                </div>
-                                                <div className="order-detail-item price-container">
-                                                    <strong className="total-label">Total<em className='price-label'>(Kshs):</em></strong>
-                                                    <strong>
-                                                    <span className="price">
-                                                        {selectedOrder.total_price ? selectedOrder.total_price.split('.').map((part, index) => (
-                                                            <React.Fragment key={index}>
-                                                                {index === 0 ? (
-                                                                    <span className="price-integer">{part}</span>
-                                                                ) : (
-                                                                    <>
-                                                                        <span style={{ fontSize: '16px' }}>.</span>
-                                                                        <span className="price-decimal">{part}</span>
-                                                                    </>
-                                                                )}
-                                                            </React.Fragment>
-                                                        )) : '0'}
-                                                    </span>
-                                                    </strong>
-                                                </div>
-                                            </div>
-                                            <h4 className='text-center'>Products</h4>
+                                        <>
+                                            <Row>
+                                                <Col xs={12} md={6} >
+                                                    <Card className="mb-2 custom-card">
+                                                        <Card.Header as="h6" className='justify-content-center'>Order ID</Card.Header>
+                                                        <Card.Body className='text-center'>
+                                                            {selectedOrder.id}
+                                                        </Card.Body>
+                                                    </Card>
+                                                </Col>
+                                                <Col xs={12} md={6}>
+                                                    <Card className="mb-2 custom-card">
+                                                        <Card.Header as="h6" className='justify-content-center'>Purchaser</Card.Header>
+                                                        <Card.Body className='text-center'>
+                                                            {selectedOrder.purchaser?.fullname || 'Unknown'}
+                                                        </Card.Body>
+                                                    </Card>
+                                                </Col>
+                                            </Row>
+                                        
+                                            <Row>
+                                                <Col xs={12} md={6}>
+                                                    <Card className="mb-2 custom-card">
+                                                        <Card.Header as="h6" className='justify-content-center'>Date Ordered</Card.Header>
+                                                        <Card.Body className='text-center'>
+                                                            {selectedOrder.order_date || 'N/A'}
+                                                        </Card.Body>
+                                                    </Card>
+                                                </Col>
+                                                <Col xs={12} md={6}>
+                                                    <Card className="mb-2 custom-card">
+                                                        <Card.Header as="h6" className='justify-content-center'>Total Price (Kshs)</Card.Header>
+                                                        <Card.Body className="price-container text-center">
+                                                            <strong>
+                                                                <span className="price">
+                                                                    {selectedOrder.total_price ? selectedOrder.total_price.split('.').map((part, index) => (
+                                                                        <React.Fragment key={index}>
+                                                                            {index === 0 ? (
+                                                                                <span className="price-integer">{part}</span>
+                                                                            ) : (
+                                                                                <>
+                                                                                    <span style={{ fontSize: '16px' }}>.</span>
+                                                                                    <span className="price-decimal">{part}</span>
+                                                                                </>
+                                                                            )}
+                                                                        </React.Fragment>
+                                                                    )) : '0'}
+                                                                </span>
+                                                            </strong>
+                                                        </Card.Body>
+                                                    </Card>
+                                                </Col>
+                                            </Row>
+                                        
+                                            <h4 className="text-center mt-2">Products</h4>
                                             <div className="product-container text-center">
                                                 <div className="table-responsive">
                                                     <Table bordered hover>
-                                                        <thead>
+                                                        <thead className='table-head'>
                                                             <tr>
                                                                 <th>Product Name</th>
                                                                 <th>Vendor</th>
@@ -279,24 +302,24 @@ const OrdersManagement = () => {
                                                                         <td>{item.product?.vendor?.fullname || 'Unknown'}</td>
                                                                         <td>{item.quantity || '0'}</td>
                                                                         <td className="price-container">
-                                                                            <em className='product-price-label'>Kshs: </em>
+                                                                            <em className="product-price-label">Kshs: </em>
                                                                             <strong>
-                                                                            <span className="price">
-                                                                                {item.product?.price && item.quantity ? (
-                                                                                    (item.product.price * item.quantity).toFixed(2).split('.').map((part, index) => (
-                                                                                        <React.Fragment key={index}>
-                                                                                            {index === 0 ? (
-                                                                                                <span className="price-integer">{part}</span>
-                                                                                            ) : (
-                                                                                                <>
-                                                                                                    <span style={{ fontSize: '16px' }}>.</span>
-                                                                                                    <span className="price-decimal">{part}</span>
-                                                                                                </>
-                                                                                            )}
-                                                                                        </React.Fragment>
-                                                                                    ))
-                                                                                ) : '0.00'}
-                                                                            </span>
+                                                                                <span className="price">
+                                                                                    {item.product?.price && item.quantity ? (
+                                                                                        (item.product.price * item.quantity).toFixed(2).split('.').map((part, index) => (
+                                                                                            <React.Fragment key={index}>
+                                                                                                {index === 0 ? (
+                                                                                                    <span className="price-integer">{part}</span>
+                                                                                                ) : (
+                                                                                                    <>
+                                                                                                        <span style={{ fontSize: '16px' }}>.</span>
+                                                                                                        <span className="price-decimal">{part}</span>
+                                                                                                    </>
+                                                                                                )}
+                                                                                            </React.Fragment>
+                                                                                        ))
+                                                                                    ) : '0.00'}
+                                                                                </span>
                                                                             </strong>
                                                                         </td>
                                                                     </tr>
@@ -310,11 +333,12 @@ const OrdersManagement = () => {
                                                     </Table>
                                                 </div>
                                             </div>
-                                        </div>
+                                        </>
                                     ) : (
                                         <p>No details available</p>
                                     )}
                                 </Modal.Body>
+
                                 <Modal.Footer>
                                     <Button variant="danger" id="button" onClick={handleCloseModal}>
                                         Close
