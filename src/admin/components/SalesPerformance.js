a72807411204a08cc3e51d85767750633d9ba966
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-08-22 13:07:10 +0300

    Done styling up the sales performance cards

--- src/admin/components/SalesPerformance.js
+++ src/admin/components/SalesPerformance.js
@@ -5,16 +5,19 @@ import { Doughnut } from 'react-chartjs-2';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const SalesPerformance = ({ data, totalRevenue }) => {
-  console.log('Sales Performance Data:', data); // Debugging line
-  console.log('Total Revenue (All Time):', totalRevenue); // Debugging line
+  console.log('Sales Performance Data:', data);
+  console.log('Total Revenue (All Time):', totalRevenue);
 
-  const months = Object.keys(data).slice(-3); // Get the last 3 months
+  // Sort months chronologically
+  const months = Object.keys(data)
+    .slice(-3)
+    .sort((a, b) => new Date(a) - new Date(b));
 
   const createChartData = (month) => {
     const monthRevenue = parseFloat(data[month]);
     const percentage = (monthRevenue / totalRevenue) * 100;
 
-    console.log(`${month} Revenue:`, monthRevenue, 'Percentage:', percentage); // Debugging line
+    console.log(`${month} Revenue:`, monthRevenue, 'Percentage:', percentage);
 
     return {
       labels: [month, 'Other'],
@@ -31,7 +34,7 @@ const SalesPerformance = ({ data, totalRevenue }) => {
   const chartOptions = {
     responsive: true,
     maintainAspectRatio: false,
-    cutout: '80%',
+    cutout: '75%',
     plugins: {
       legend: {
         display: false,
@@ -49,10 +52,10 @@ const SalesPerformance = ({ data, totalRevenue }) => {
   };
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', height: '300px' }}>
+    <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center', height: 'auto' }}>
       {months.map((month) => (
         <div key={month} style={{ width: '30%', height: '100%', textAlign: 'center' }}>
-          <div style={{ height: '200px' }}>
+          <div>
             <Doughnut key={`${month}-chart`} data={createChartData(month)} options={chartOptions} />
           </div>
           <p style={{ marginTop: '10px' }}>{month}</p>
