551bcea1705c4aad7f1501e78be0be8d16bc6f8e
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-09-02 16:07:59 +0300

    Creates a new styling for the topbar nav

--- src/purchaser/components/TopNavbar.js
+++ src/purchaser/components/TopNavbar.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
+import { List } from 'react-bootstrap-icons';
 import './TopNavbar.css';
 
-const TopNavbar = () => {
+const TopNavbar = ({ onSidebarToggle, sidebarOpen }) => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" className="fixed-top navbar">
       <Container fluid>
+        <Button 
+          variant="warning" 
+          className={`toggle-button ${sidebarOpen ? 'd-none' : 'd-block'}`} 
+          onClick={onSidebarToggle}
+        >
+          <List size={15} /> {/* Sidebar toggle icon */}
+        </Button>
         <Navbar.Brand href="/purchaser/homepage">CARBON - Purchaser</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
