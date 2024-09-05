6d077647dec02407510642a68c750906c57c5090
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-09-05 11:14:51 +0300

    Updates the side bar icons

--- src/purchaser/components/Sidebar.js
+++ src/purchaser/components/Sidebar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Nav, Button } from 'react-bootstrap';
-import { House, Box, Truck, Person, FileText, GraphUp, XCircle, ArrowRight, List } from 'react-bootstrap-icons';
+import { BookmarkDash, Person, XCircle, ArrowRight, Cart, Bell, ChatSquareText, BagCheck, HouseGear } from 'react-bootstrap-icons';
 import { useLocation } from 'react-router-dom';
 import './Sidebar.css';
 
@@ -27,37 +27,37 @@ const Sidebar = () => {
           <Nav.Link
             href="/purchaser/homepage"
             className={location.pathname === '/purchaser/homepage' ? 'active' : ''}>
-            <GraphUp className="icon" /> {isOpen && 'HomePage'}
+            <HouseGear className="icon" /> {isOpen && 'HomePage'}
           </Nav.Link>
           <Nav.Link
             href="/purchaser/orders-history"
             className={location.pathname === '/purchaser/orders-history' ? 'active' : ''}>
-            <Truck className="icon" /> {isOpen && 'Orders'}
+            <BagCheck className="icon" /> {isOpen && 'Orders'}
           </Nav.Link>
           <Nav.Link
             href="/purchaser/bookmarks"
             className={location.pathname === '/purchaser/bookmarks' ? 'active' : ''}>
-            <Box className="icon" /> {isOpen && 'Bookmarks'}
+            <BookmarkDash className="icon" /> {isOpen && 'Bookmarks'}
           </Nav.Link>
           <Nav.Link
             href="/purchaser/shopping-cart"
             className={location.pathname === '/purchaser/shopping-cart' ? 'active' : ''}>
-            <List className="icon" /> {isOpen && 'Shopping Cart'}
+            <Cart className="icon" /> {isOpen && 'Shopping Cart'}
           </Nav.Link>
           <Nav.Link
             href="/purchaser/messages"
             className={location.pathname === '/purchaser/messages' ? 'active' : ''}>
-            <House className="icon" /> {isOpen && 'Messages'}
+            <ChatSquareText className="icon" /> {isOpen && 'Messages'}
           </Nav.Link>
           <Nav.Link
             href="/purchaser/notifications"
             className={location.pathname === '/purchaser/notifications' ? 'active' : ''}>
-            <Person className="icon" /> {isOpen && 'Notifications'}
+            <Bell className="icon" /> {isOpen && 'Notifications'}
           </Nav.Link>
           <Nav.Link
             href="/purchaser/profile"
             className={location.pathname === '/purchaser/profile' ? 'active' : ''}>
-            <FileText className="icon" /> {isOpen && 'Profile'}
+            <Person className="icon" /> {isOpen && 'Profile'}
           </Nav.Link>
         </Nav>
       </div>
