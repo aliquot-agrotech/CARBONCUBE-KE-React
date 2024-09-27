1566744dedb8407939694de13abc29d1656d862a
Lewis Kathembe <lewiskathembe51@gmail.com>
2024-09-27 15:28:42 +0300

    Updates the log-out button

--- src/vendor/components/TopNavbar.js
+++ src/vendor/components/TopNavbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
 import './TopNavbar.css';
 
 const TopNavbar = () => {
@@ -11,7 +11,9 @@ const TopNavbar = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             {/* <Nav.Link href="/profile">Profile</Nav.Link> */}
-            <Nav.Link href="/login">Logout</Nav.Link>
+            <Button variant="warning" href="/login" className="ms-2" id="button">
+                Logout
+            </Button>
           </Nav>
         </Navbar.Collapse>
       </Container>
