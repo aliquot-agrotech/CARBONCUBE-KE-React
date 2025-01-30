a44500602807704b04ecbc50b7686d9f72e4a5c8
LewKM <lewiskathembe51@gmail.com>
2025-01-30 16:11:34 +0300

    Adds the new top wishlisted card

--- src/vendor/components/TopWishListedAds.js
+++ src/vendor/components/TopWishListedAds.js
@@ -15,6 +15,27 @@ const TopWishListedAds = ({ data }) => {
             />
             <Card.Body className="analytics-card-body p-0 mx-2">
               <Card.Title className='d-flex justify-content-start mb-0' style={{ fontSize: '17px' }}>{ad.ad_title}</Card.Title>
+              <Card.Text className="analytics-price-container justify-content-start mt-0">
+                <em className="text-success" style={{ fontSize: '12px' }}>Price: </em>
+                <strong style={{ fontSize: '18px' }} className="text-danger">
+                  {ad.ad_price && ad.ad_price.split('.').map((part, index) => (
+                    <React.Fragment key={index}>
+                      {index === 0 ? (
+                        <span className="analytics-price-integer">
+                          {parseInt(part, 10).toLocaleString()} {/* Format integer with commas */}
+                        </span>
+                      ) : (
+                        <>
+                          <span style={{ fontSize: '10px' }}>.</span>
+                          <span className="analytics-price-decimal">
+                            {(part || '00').padEnd(2, '0').slice(0, 2)} {/* Ensure two decimal points */}
+                          </span>
+                        </>
+                      )}
+                    </React.Fragment>
+                  ))}
+                </strong>
+              </Card.Text>
               <Card.Text className="d-flex justify-content-between align-items-center">
                 <strong>Wishlisted:&nbsp;</strong> 
                 <span className="badge bg-primary">{ad.wishlist_count} wishes</span>
