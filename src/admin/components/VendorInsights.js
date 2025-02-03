68d1d3e3003b8ef265c349c10ae404a7fef891e8
LewKM <lewiskathembe51@gmail.com>
2025-02-03 15:24:35 +0300

    Adds new vendor insights data and display logic

--- src/admin/components/VendorInsights.js
+++ src/admin/components/VendorInsights.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Table, Form } from 'react-bootstrap';
 
 const VendorInsights = () => {
-  const [selectedMetric, setSelectedMetric] = useState('Total Orders');
+  const [selectedMetric, setSelectedMetric] = useState('Rating');
   const [vendorsData, setVendorsData] = useState([]);
   const [loading, setLoading] = useState(false);
 
@@ -51,9 +51,10 @@ const VendorInsights = () => {
                   value={selectedMetric}
                   onChange={handleMetricChange}
                 >
-                  <option>Total Orders</option>
-                  <option>Total Revenue</option>
                   <option>Rating</option>
+                  <option>Total Ads</option>
+                  <option>Reveal Clicks</option>
+                  <option>Ad Clicks</option>
                 </Form.Control>
               </th>
             </tr>
@@ -64,40 +65,24 @@ const VendorInsights = () => {
                 <td>{index + 1}</td>
                 <td>{vendor.fullname}</td>
                 <td>
-                  {selectedMetric === 'Total Orders' && (
-                    <strong className=" text-success fw-bold">{vendor.total_orders}</strong>
+                  {selectedMetric === 'Rating' && (
+                    <strong className=" text-success fw-bold">{vendor.mean_rating ? parseFloat(vendor.mean_rating).toFixed(2) : '0.00'}</strong>
                   )}
-                  {selectedMetric === 'Total Revenue' && (
+                  {selectedMetric === 'Total Ads' && (
                     <>
-                      {/* <span className="text-success">
-                        <em style={{ fontSize: '13px' }}>Kshs: &nbsp;</em>
-                      </span> */}
                       <strong style={{ fontSize: '16px' }} className="text-success">
-                        {Number(vendor.total_revenue || 0)
-                          .toFixed(2)
-                          .split('.')
-                          .map((part, index) => (
-                            <React.Fragment key={index}>
-                              {index === 0 ? (
-                                <span className="analytics-price-integer">
-                                  {parseInt(part, 10).toLocaleString()} {/* Add commas to the integer part */}
-                                </span>
-                              ) : (
-                                <>
-                                  <span style={{ fontSize: '10px' }}>.</span>
-                                  <span className="analytics-price-decimal">
-                                    {(part || '00').padEnd(2, '0').slice(0, 2)} {/* Ensure two decimal points */}
-                                  </span>
-                                </>
-                              )}
-                            </React.Fragment>
-                          ))}
+                        {vendor.total_ads}
                       </strong>
                     </>
                   )}
-                  {selectedMetric === 'Rating' && (
+                  {selectedMetric === 'Reveal Clicks' && (
                     <strong className="text-success fw-bold">
-                      {vendor.mean_rating ? parseFloat(vendor.mean_rating).toFixed(2) : '0.00'}
+                      {vendor.reveal_clicks}
+                    </strong>
+                  )}
+                  {selectedMetric === 'Ad Clicks' && (
+                    <strong className="text-success fw-bold">
+                      {vendor.total_ad_clicks}
                     </strong>
                   )}               
                 </td>
