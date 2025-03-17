239b77d3a20a1ed9ead1c640aba9ba05e010ee18
Lewis Kathembe <lewiskathembe51@gmail.com>
2025-03-17 16:13:17 +0300

    Adds new styling to the image components

--- src/purchaser/pages/Home.js
+++ src/purchaser/pages/Home.js
@@ -320,6 +320,7 @@ const Home = () => {
                                         <Card.Img 
                                             variant="top" 
                                             src={ad.media_urls && ad.media_urls.length > 0 ? ad.media_urls[0] : 'default-image-url'}
+                                            alt={ad.title}
                                             className="ad-image"
                                             onClick={() => onAdClick(ad.id)} 
                                         />
