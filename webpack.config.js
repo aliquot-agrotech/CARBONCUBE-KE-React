8876216cf070ffcc04b8688ab1ffd5d195d56d5e
Lewis Kathembe <lewiskathembe51@gmail.com>
2025-06-12 15:05:17 +0300

    Adds new code for the webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
 const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
-  module: {
-    rules: [
-      ...(isDev
-        ? []
-        : [
-            {
-              test: /\.js$/,
-              enforce: 'pre',
-              loader: 'source-map-loader',
-              exclude: /node_modules/,
-            },
-          ]),
-    ],
-  },
-  ...(isDev && {
-    devServer: {
-      hot: true,
-      liveReload: false,
-      watchOptions: {
-        aggregateTimeout: 300,
-        poll: 1000,
-        ignored: /node_modules/,
-      },
+    module: {
+        rules: [
+        ...(isDev
+            ? []
+            : [
+                {
+                test: /\.js$/,
+                enforce: 'pre',
+                loader: 'source-map-loader',
+                exclude: /node_modules/,
+                },
+            ]),
+        ],
     },
-    optimization: {
-      removeAvailableModules: false,
-      removeEmptyChunks: false,
-      splitChunks: false,
-    },
-  }),
+    ...(isDev && {
+        devServer: {
+        hot: true,
+        liveReload: false,
+        watchOptions: {
+            aggregateTimeout: 300,
+            poll: 1000,
+            ignored: /node_modules/,
+        },
+        },
+        optimization: {
+        removeAvailableModules: false,
+        removeEmptyChunks: false,
+        splitChunks: false,
+        },
+    }),
 };
